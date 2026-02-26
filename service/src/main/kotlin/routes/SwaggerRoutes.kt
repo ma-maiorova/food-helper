28e6c5695f1ab38ea11db2cb2804b46835885c08
Marina <ma.maiorova@gmail.com>
2026-02-27 01:46:35 +0300

    fix routes

--- service/src/main/kotlin/routes/SwaggerRoutes.kt
+++ service/src/main/kotlin/routes/SwaggerRoutes.kt
@@ -1,6 +1,5 @@
 package org.example.routes
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,21 +8,10 @@ import io.ktor.server.http.content.*
 fun Application.swaggerRoutes() {
     routing {
         staticResources("/openapi", "openapi")
+        staticResources("/swagger", "swagger-ui")
 
         get("/swagger") {
-            call.respondRedirect("/swagger-ui/index.html", permanent = false)
-        }
-        get("/swagger-ui") {
-            call.respondRedirect("/swagger-ui/index.html", permanent = false)
-        }
-        get("/swagger-ui/") {
-            call.respondRedirect("/swagger-ui/index.html", permanent = false)
-        }
-        get("/swagger-ui/index.html") {
-            val stream = call.application.javaClass.classLoader.getResourceAsStream("swagger-ui/index.html")
-                ?: return@get call.respond(HttpStatusCode.NotFound)
-            val bytes = stream.readBytes()
-            call.respondBytes(bytes, ContentType.Text.Html)
+            call.respondRedirect("/swagger/index.html", permanent = false)
         }
     }
 }

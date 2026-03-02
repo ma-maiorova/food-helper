ec6e984f9497b30c9bc7039874221205896a0298
Marina <ma.maiorova@gmail.com>
2026-03-02 12:00:53 +0300

    add import endpoint

--- service/src/main/kotlin/api/routes/ApiRoutes.kt
+++ service/src/main/kotlin/api/routes/ApiRoutes.kt
@@ -3,17 +3,20 @@ package org.example.api.routes
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.example.service.DeliveryServiceService
+import org.example.service.ProductImportService
 import org.example.service.ProductService
 
 fun Application.configureApiRoutes(
     deliveryServiceService: DeliveryServiceService,
-    productService: ProductService
+    productService: ProductService,
+    productImportService: ProductImportService
 ) {
     routing {
         route("/api/v1") {
             deliveryServiceRoutes(deliveryServiceService)
             productRoutes(productService)
             adminDeliveryServiceRoutes(deliveryServiceService)
+            adminImportRoutes(productImportService)
         }
     }
 }

f11cb4a314332896ec28432b9cc6f7dea548766a
ezik <ezvtreugolnike@gmail.com>
2026-03-22 23:05:32 +0300

    fix: revert swagger routing

--- service/src/main/kotlin/routes/SwaggerRoutes.kt
+++ service/src/main/kotlin/routes/SwaggerRoutes.kt
@@ -1,14 +1,17 @@
 package org.example.routes
 
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.http.content.*
 
 fun Application.swaggerRoutes() {
     routing {
         staticResources("/openapi", "openapi")
-        staticResources("/swagger", "swagger-ui") {
-            default("index.html")
+        // redirect must be before staticResources so Ktor reaches it
+        get("/swagger") {
+            call.respondRedirect("/swagger/index.html", permanent = false)
         }
+        staticResources("/swagger", "swagger-ui")
     }
 }

b4d2c9abfd03f3f5415b4f2f5dc04606138ceb73
ma-maiorova <ma.maiorova@gmail.com>
2026-03-22 22:58:01 +0300

    fix

--- service/src/main/kotlin/routes/SwaggerRoutes.kt
+++ service/src/main/kotlin/routes/SwaggerRoutes.kt
@@ -1,17 +1,14 @@
 package org.example.routes
 
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.http.content.*
 
 fun Application.swaggerRoutes() {
     routing {
         staticResources("/openapi", "openapi")
-        staticResources("/swagger", "swagger-ui")
-
-        get("/swagger") {
-            call.respondRedirect("/swagger/index.html", permanent = false)
+        staticResources("/swagger", "swagger-ui") {
+            default("index.html")
         }
     }
 }

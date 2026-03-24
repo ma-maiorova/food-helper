81e803bd1c8d520629e7378ec2af376a951a9aa7
Скогорева Анна Сергеевна <asskogoreva@edu.hse.ru>
2026-03-25 00:30:57 +0300

    add db uploader

---
+++ parser_v3/utils/browser.py
@@ -0,0 +1,26 @@
+# utils/browser.py
+import undetected_chromedriver as uc
+
+
+def get_driver(logger, headless: bool = True):
+    """
+    Создает и возвращает экземпляр Chrome через undetected_chromedriver.
+    """
+    logger.info("Создаю драйвер...")
+    options = uc.ChromeOptions()
+
+    options.add_argument("--headless=new")
+    options.add_argument(
+        "--disable-blink-features=AutomationControlled")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--lang=ru-RU")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = uc.Chrome(options=options, version_main=145)
+    driver.set_page_load_timeout(30)
+    logger.info("Драйвер успешно работает")
+    return driver
